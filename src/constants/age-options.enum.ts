681a6f807003e41f669448c4e3792800543584ae
Sergey Denwer <sergey.vlasov@coch.org>
2024-05-20 13:35:53 +0400

    update

--- src/constants/age-options.enum.ts
+++ src/constants/age-options.enum.ts
@@ -3,6 +3,6 @@ export enum AgeOptions {
   Between18And25 = "Между 20 и 25 годами",
   Between25And35 = "Между 25 и 35 годами",
   Between35And45 = "Между 35 и 45 годами",
-  Between45And55 = "Между 35 и 55 годами",
+  Between45And55 = "Между 45 и 55 годами",
   OlderThan55 = "Старше 55 лет"
 }
