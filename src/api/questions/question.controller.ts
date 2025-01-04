19f8fc9f0a590edd7e1e09782bc108edf68291f1
Sergey Denwer <sergey.vlasov@coch.org>
2025-01-04 13:04:28 +0400

    create api for questions

--- src/api/questions/question.controller.ts
+++ src/api/questions/question.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, UnauthorizedException, Headers } from '@nestjs/common';
 import { QuestionsService } from '../../surveys/questions.service';
 
 @Controller('api')
@@ -6,19 +6,14 @@ export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
   @Get('recent-surveyed-news')
-  async getRecentSurveyedNews(@Query('token') token: string): Promise<object[]> {
-    // Проверяем токен
-    const API_TOKEN = process.env.API_TOKEN; // Токен из переменных окружения
+  async getRecentSurveyedNews(@Headers('Authorization') authHeader: string): Promise<object[]> {
+    const API_TOKEN = process.env.API_TOKEN;
+    const token = authHeader?.split(' ')[1];
     if (token !== API_TOKEN) {
       throw new UnauthorizedException('Invalid API token');
     }
 
-    // Получаем последние 20 вопросов
-    const questions = await this.questionsService.getRecentSurveyedQuestions();
-    return questions.map((question) => ({
-      id: question.id,
-      created_at: question.created_at.toISOString(),
-      question: question.question,
-    }));
+    return this.questionsService.getRecentSurveyedQuestions();
   }
+
 }
